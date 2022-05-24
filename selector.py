6948deebe378ea32a28afbacd53036dbd07715df
Jeong Ju, Kim <haje01@gmail.com>
2022-05-24 18:58:23 +0900

    fix bug

--- selector.py
+++ selector.py
@@ -10,16 +10,12 @@ assert num_arg in (2, 3)
 dev = num_arg == 2
 setup = sys.argv[1]
 pid = int(sys.argv[2]) if not dev else -1
-epoch = int(sys.argv[3]) if not dev else -1
-pid = int(sys.argv[2]) if not dev else -1
 
 
 with open(setup, 'rt') as f:
     setup = json.loads(f.read())
 
 print(f"Dev: {dev}")
-print(f"Epoch: {epoch}")
-print(f"Batch: {batch}")
 ip = setup['sqlserver_public_ip'] if dev else setup['sqlserver_private_ip']
 SERVER = ip['value']
 # SERVER = setup['sqlserver_public_ip']['value']
@@ -41,24 +37,24 @@ def count_rows():
     return res['cnt']
 
 sql = f'''
-    SELECT TOP {10000} *
+    SELECT TOP {100000} *
     FROM [test].[dbo].[person]
     ORDER BY newid()
     '''
 
 st = time.time()
-i = 0
-prev_cnt = count_rows()
+cnt = i = 0
+prev = count_rows()
 while True:
     i += 1
     time.sleep(1)
-    print(f"Epoch: {i}")
+    print(f"Epoch: {i} - cnt: {cnt}, prev: {prev}")
     cursor.execute(sql)
     cursor.fetchall()
     cnt = count_rows()
-    if cnt == prev_cnt:
+    if cnt == prev:
         break
-    prev_cnt = cnt
+    prev = cnt
 
 conn.close()
 
