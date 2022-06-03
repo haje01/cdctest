82ebed0870b250da8786f1010bf656f3bc4e5df8
Ubuntu <ubuntu@ip-172-31-49-228.ap-northeast-2.compute.internal>
2022-06-03 08:30:05 +0000

    consumer

--- mysql_jdbc/consumer.py
+++ mysql_jdbc/consumer.py
@@ -1,4 +1,3 @@
-import pdb
 import sys
 import json
 
@@ -10,35 +9,20 @@ setup = sys.argv[1]
 with open(setup, 'rt') as f:
     setup = json.loads(f.read())
 
-kafka_ip = setup['kafka_private_ip'].value
+kafka_ip = setup['kafka_private_ip']['value']
 
-max_cnt = None
-if len(sys.argv) == 2:
-    max_cnt = int(sys.argv[1])
-
-consumer1 = KafkaConsumer('my_topic_person',
+consumer = KafkaConsumer('my_topic_person',
                          group_id='my-group',
                          bootstrap_servers=[f'{kafka_ip}:9092'],
                          auto_offset_reset='earliest',
-                         enable_auto_commit=False
+                         enable_auto_commit=False,
+                         consumer_timeout_ms=10000,
                          )
 
-consumer2 = KafkaConsumer('my_topic_person',
-                         group_id='my-group',
-                         bootstrap_servers=[f'{kafka_ip}:9092'],
-                         auto_offset_reset='earliest',
-                         enable_auto_commit=False
-                         )
-
-print("connected")
+print("Connected")
 cnt = 0
-for msg1, msg2 in zip(consumer1, consumer2):
+for msg in consumer:
     cnt += 1
-    if max_cnt is not None:
-        if cnt == max_cnt:
-            break
-    else:
-        print(f'{msg.topic}:{msg.partition}:{msg.offset} key={msg.key} value={msg.value}')
+    print(f'{msg.topic}:{msg.partition}:{msg.offset} key={msg.key} value={msg.value}')
 
-if max_cnt is not None:
-    print(cnt)
+print(f"Total: {cnt}")
